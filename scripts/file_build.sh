714de7c85c1a63fd2ca4633eb5177c5c7ed3a1e0
fixxxer23 <acswork@mail.ru>
2023-03-13 19:48:26 +0200

    Revert "magic_mgc.h reverted"
    This reverts commit ad065beedbccdea2c7bd906a9c5bfec74fedd639.

--- scripts/file_build.sh
+++ scripts/file_build.sh
@@ -9,16 +9,12 @@ make
 
 ##Copy native binaries to native
 mkdir native
-cp ./src/file ./native/
-cp ./magic/magic.mgc ./native/
+cp ./src/file ./native/file
 cd native
 mkdir .libs
 cd ..
-cp -Lr ./src/.libs/file ./native/.libs/
+cp -Lr ./src/.libs/file ./native/.libs/file
 cp -Lr ./src/.libs/libmagic.so.* ./native/.libs/
-
-##Convert magic.mgc to header file
-xxd -i ./native/magic.mgc > ./src/magic_mgc.h
 
 ##Build WASM
 # Run emconfigure with the normal configure command as an argument.
