ad065beedbccdea2c7bd906a9c5bfec74fedd639
fixxxer23 <acswork@mail.ru>
2023-03-13 19:37:54 +0200

    magic_mgc.h reverted

--- scripts/file_build.sh
+++ scripts/file_build.sh
@@ -9,12 +9,16 @@ make
 
 ##Copy native binaries to native
 mkdir native
-cp ./src/file ./native/file
+cp ./src/file ./native/
+cp ./magic/magic.mgc ./native/
 cd native
 mkdir .libs
 cd ..
-cp -Lr ./src/.libs/file ./native/.libs/file
+cp -Lr ./src/.libs/file ./native/.libs/
 cp -Lr ./src/.libs/libmagic.so.* ./native/.libs/
+
+##Convert magic.mgc to header file
+xxd -i ./native/magic.mgc > ./src/magic_mgc.h
 
 ##Build WASM
 # Run emconfigure with the normal configure command as an argument.
